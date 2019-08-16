949a978efe8fd38b85c5d170d84ef75e52b18a77
huangshm23 <1031686129@qq.com>
2019-08-16 23:46:05 +0800

    changchang

---
+++ front-end/src/components/Register.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="register">
+    <h1>{{ msg }}</h1>
+    <div id="form">
+      <label for="account">Account：</label>
+      <input type="text" id="account" v-model.trim="account" @blur="jiaoyan1"><br>
+      <div v-if="!jiaoyanbiaoji1">6到8位大小写字母</div>
+      <label for="age">Password：</label>
+      <input type="password" id="password" v-model.trim="password" @blur="jiaoyan2"><br>
+      <div v-if="!jiaoyanbiaoji2">6到8位数字</div>
+      <router-link :to="{ path: '/signin' }">登录</router-link>
+      <br>
+      <div v-if="biaoji == 2" @click="submit">提交</div>
+      <div v-else>待提交</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Register',
+  data () {
+    return {
+      jiaoyanbiaoji1:true,
+      jiaoyanbiaoji2:true,
+      biaoji:0,
+      account:'',
+      password:'',
+      msg: 'register'
+    }
+  },
+  methods:{
+    submit:function(){
+      console.log(this.biaoji);
+      //把account，password发送给后端
+      //后端返回状态字
+      //0：注册成功，跳转个人主页
+      //1:账号已存在
+    },
+    jiaoyan1:function(){
+      //校验账号
+      console.log("jiaoyan1");
+      var par1=/^[a-zA-Z]{6,10}$/;
+      var a=this.jiaoyanbiaoji1;
+      this.jiaoyanbiaoji1=par1.test(this.account);
+      if(!a && this.jiaoyanbiaoji1)
+        this.biaoji++;
+      if(this.biaoji>0 && a && !this.jiaoyanbiaoji1)
+        this.biaoji--;
+      console.log(this.jiaoyanbiaoji1);
+    },
+    jiaoyan2:function(){
+      //校验密码
+      console.log("jiaoyan2");
+      var par2=/^[0-9]{6,10}$/;
+      var a=this.jiaoyanbiaoji2;
+      this.jiaoyanbiaoji2=par2.test(this.password);
+      if(!a && this.jiaoyanbiaoji2)
+        this.biaoji++;
+      if(this.biaoji>0 && a && !this.jiaoyanbiaoji2)
+        this.biaoji--;
+      console.log(this.jiaoyanbiaoji2);
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -1,15 +1,27 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import signin from '@/components/Signin'
+import register from '@/components/Register'
 
 Vue.use(Router)
 
 export default new Router({
+  mode:'history',
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'signin',
+      component: signin
+    },
+    {
+      path: '/signin',
+      name: 'signin',
+      component: signin
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: register
     }
   ]
 })

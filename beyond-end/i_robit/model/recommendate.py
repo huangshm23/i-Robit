73baaad33a4142aa90b102bbc9507ad68711a72f
gmhdbjd <gmhdbjd@gmail.com>
2019-08-21 04:54:19 -0700

    update model

--- beyond-end/i_robit/model/recommendate.py
+++ beyond-end/i_robit/model/recommendate.py
@@ -1,2 +1,10 @@
 def recommendate(questionnaire):
-    return {'test':'test'}
+    result = {
+        'recommendation': {
+            '中石油': 0.15,
+            '中石化': 0.6
+        },
+        'expected_rate': 0.2,
+        'risk_factor': 1.5
+    }
+    return result

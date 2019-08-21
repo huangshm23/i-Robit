40b290b5fac418a9a4dc4c35063d4175a170c680
gmhdbjd <gmhdbjd@gmail.com>
2019-08-21 03:20:54 -0700

    add csrf

--- beyond-end/i_robit/i_robot/views.py
+++ beyond-end/i_robit/i_robot/views.py
@@ -8,6 +8,7 @@ from model.recommendate import recommendate
 from model.simulation import simulation
 import json
 from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
 
 
 # Create your views here.

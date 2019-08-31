6936f770a74157f3f8acdc1b5fabe3d2480a9537
Assiduous-donkey <40432256+Assiduous-donkey@users.noreply.github.com>
2019-08-31 10:56:30 +0800

    Add files via upload

--- beyond-end/i_robit/i_robit/urls.py
+++ beyond-end/i_robit/i_robit/urls.py
@@ -14,10 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path,re_path
+from django.views.generic.base import TemplateView
+from users import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #用户系统的URL
-    path('users/',include('users.urls',namespace='users')),
+    #连接前端代码
+    path('',TemplateView.as_view(template_name="index.html")),
+
+    path('login/',views.Login.as_view(),name='login'),
+    #注销
+    path('logout/',views.Logout.as_view(),name='logout'),
+    #注册
+    path('register/',views.Register.as_view(),name='register'),
+    re_path('activate/(?P<activate_id>\w+)',views.Activate.as_view()),
 ]

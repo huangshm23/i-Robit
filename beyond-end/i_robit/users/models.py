b0cc3cdef46715bf59da43da343df3866d9c8481
Assiduous-donkey <40432256+Assiduous-donkey@users.noreply.github.com>
2019-08-31 10:51:54 +0800

    token验证

--- beyond-end/i_robit/users/models.py
+++ beyond-end/i_robit/users/models.py
@@ -1,13 +1,21 @@
+
 from django.db import models
 
+
+
 # Create your models here.
-'''
+
+
+
 class User(models.Model):
-    gender_choice = (('male','男'),('female','女'),)
+
+    #gender_choice = [('male','男'),('female','女'),]
     username = models.CharField(max_length=64,unique=True)
-    password = models.CharField(max_length=12)
-    email = models.EmailField(unique=True)
-    gender = models.CharField(max_length=10,choices=gender_choice,default='男')
+    password = models.CharField(max_length=256)
+    is_active = models.BooleanField(default=False)
+    token = models.CharField(max_length=128,null=True,blank=True)
+    #email = models.EmailField(unique=True)
+    #gender = models.CharField(max_length=10,choices=gender_choice,default='男')
 
     def __str__(self):
         return self.username
@@ -15,4 +23,22 @@ class User(models.Model):
     class Meta:
         verbose_name = '用户'
         verbose_name_plural = '用户'
-'''
+
+class UserToken(models.Model):
+    token = models.CharField(max_length=128)
+    user = models.OneToOneField(to='User',on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.token
+
+class Email_auth(models.Model):
+    '''用于邮箱验证'''
+    activate_id = models.CharField(max_length=8)
+    email = models.CharField(max_length=32)
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name = '邮箱验证'
+        verbose_name_plural = verbose_name
--- beyond-end/i_robit/users/views.py
+++ beyond-end/i_robit/users/views.py
@@ -1,62 +1,124 @@
-from django.shortcuts import render
-from django.http import JsonResponse,HttpResponseRedirect
-from django.urls import reverse
-from django.contrib.auth import logout,login,authenticate
-from django.contrib.auth.forms import UserCreationForm
-import json
+from django.http import JsonResponse
+from django.contrib.auth import logout
+from django.core.mail import send_mail
+from django.contrib.auth.hashers import make_password, check_password
+from .models import User,Email_auth,UserToken
+# 装饰器
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
 
-from .forms import LoginForm
-# Create your views here.
+#rest_framework
+from rest_framework.views import APIView
 
-def login_view(request):
+#自定义
+from utils.auth import Generete_token,Authtication,get_activate_id
+
+
+@method_decorator(csrf_exempt, name='dispatch')
+class Login(APIView):
     '''登录视图'''
-    if request.method == 'POST':
-        login_form = LoginForm(request.POST)
-
-        if login_form.is_valid():#验证是否符合表单的要求
-            username = request.POST.get('username')
-            passsword = request.POST.get('password')
-            user = authenticate(username=username,passsword=passsword)
-            #match:0-匹配成功;1-账号不存在;2-密码错误
-            if user:#验证用户名密码是否匹配
-                login(request,user)
-                match = 0
-            else:
-                match = 2
-            #前端要求的数据：用户名、密码、是否匹配
-            context = {'status':match}
-            return JsonResponse(context) 
+    def post(self,request):
+        context = {}
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        count = User.objects.filter(username=username).count()
+        if count == 0:
+            context = {'status':1}
+            print('账号不存在')
         else:
-            pass
-    else:   #不是POST请求就返回一个空表单
-        context = {'status':1}
+            user = User.objects.get(username=username)
+            if user.is_active == False:
+                context = {'status':2}    #账号未激活
+                print('账号未激活')
+            else:
+                if check_password(password,user.password):
+                    context = {'status':0}
+                    print('密码正确')
+                    token = Generete_token(username)
+                    print('生成token: ',token)
+                    context['token']=token
+                    user.token = token
+                    user.save()
+                    UserToken.objects.update_or_create(user=user,defaults={'token':token})
+                else:
+                    context = {'status':1}  #账户或密码错误
+                    print('密码错误')
         return JsonResponse(context)
+    
+    def get(self,request):
+        return JsonResponse({})
 
-def logout_view(request):
+
+@method_decorator(csrf_exempt, name='dispatch')
+class Logout(APIView):
     '''注销视图'''
-    logout(request)
-    context = {'status':'exit'}
-    return JsonResponse(context)
+    authentication_classes = [Authtication,]
+    def get(self,request):
+        print(request.user)
+        print(request.auth)
+        logout(request) #注销函数
+        #删除用户对应的Token
+        token = request._request.GET.get('token')
+        token_obj = UserToken.objects.get(token=token)
+        token_obj.delete()
+        return JsonResponse({'status':0})
 
-def register_view(request):
+
+@method_decorator(csrf_exempt, name='dispatch')
+class Register(APIView):
     '''注册视图'''
-    if request.method != 'POST':
-        form = UserCreationForm()
-    else:
-        form = UserCreationForm(data=request.POST)
-        if form.is_valid():  #表单验证
-            new_user = form.save()
-            username = new_user.username
-            password = request.POST['password1']
-            authenticated_user = authenticate(username=username,
-            password=password)
-            if authenticated_user is not None:
-                login(request,authenticated_user)
-                context = {'status':0}  # 0:成功
-                return JsonResponse(context)
-    context = {'status':1}  #1:账号已存在
-    return JsonResponse(context)
+    def post(self,request):
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        count = User.objects.filter(username=username).count()
+
+        #先生成可能要发送的邮件信息
+        email_to = username
+        activate_id = get_activate_id()
+        title = 'i-Robot注册验证'
+        #message = '欢迎注册irobot，请点击此链接激活账号：http://178.128.115.175:80/activate/{0}'.format(activate_id)
+        message = '欢迎注册irobot，请点击此链接激活账号：http://127.0.0.1:8000/activate/{0}'.format(activate_id)
+        #email_from = 'irobot2019@sina.com'
+        email_from = '986439206@qq.com'
+        reciever = [email_to]    
+        
+        count = User.objects.filter(username=username).count()
+        if count!=0:
+            user = User.objects.get(username=username)
+            if user.is_active == True:  #已经注册且激活
+                print('账号已经注册好了')
+                return JsonResponse({'status':1,'msg':'用户名已存在'})
+
+        #未注册 或者 未激活 都要发送激活邮件
+        try:
+            sendstatus = send_mail(title,message,email_from,reciever)
+            print('邮件发送状态:',sendstatus)
+            Email_auth.objects.create(activate_id=activate_id,email=email_to)
+        except:
+            return JsonResponse({'status':2,'msg':'邮箱不存在'})
+        #如果是未注册，这是已经验证邮箱合法，可以将用户信息存进数据库
+        if count == 0:
+            print('创建账号但未激活')
+            User.objects.create(username=username,password=make_password(password,None,'pbkdf2_sha1'),is_active=False)
+        else:
+            print('账号已经创建但未激活')
+        return JsonResponse({'status':0,'msg':''})
+
+    def get(self,request):
+        return JsonResponse({})
 
 
-
-
+@method_decorator(csrf_exempt, name='dispatch')
+class Activate(APIView):
+    '''账户激活视图'''
+    def get(self,request,activate_id):
+        print('activate_id: ',activate_id)
+        Email_user = Email_auth.objects.filter(activate_id=activate_id).first()
+        if Email_user is not None:
+            username = Email_user.email
+            user = User.objects.get(username=username)
+            user.is_active = True
+            user.save()
+            Email_user.delete()
+            return JsonResponse({'status':0})   #激活成功
+        return JsonResponse({'status':1})           #激活失败

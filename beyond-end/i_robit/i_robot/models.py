0f4da9cf74af5e02a314863fd08c9595be0da201
Assiduous-donkey <40432256+Assiduous-donkey@users.noreply.github.com>
2019-09-15 10:34:19 +0800

    增加算法模型

--- beyond-end/i_robit/i_robot/models.py
+++ beyond-end/i_robit/i_robot/models.py
@@ -9,3 +9,18 @@ class News(models.Model):
     time = models.DateTimeField()
     source = models.CharField(max_length=255)
     newspaper = models.TextField()
+
+class Fund(models.Model):
+    fundname = models.CharField(max_length=64, unique=True)
+    fundno = models.CharField(max_length=24,null=True,blank=True)
+    profit = models.FloatField()
+
+    def __str__(self):
+        return self.fundname
+
+
+class Combination(models.Model):
+    risk_factor = models.FloatField(primary_key=True)
+    recommendation = models.CharField(max_length=1024)
+    expected_rate = models.FloatField()
+    std = models.FloatField()
